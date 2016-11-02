da63dd3bda757a8aca15c372cff7741d663f57ab
Лукиенко Илья <lukienkoilya@gmail.com>
2016-11-02 22:52:45 +0200

    Initial commit

---
+++ src/main/java/com/nure/lukienkoilya/User.java
@@ -0,0 +1,60 @@
+package main.java.com.nure.lukienkoilya;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Илья on 09.10.2016.
+ */
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirthday;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirthday() {
+        return dateOfBirthday;
+    }
+
+    public void setDateOfBirthday(Date dateOfBirthday) {
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public Object getFullName() {
+        return getLastName() + ", " + getFirstName();
+    }
+
+    public int getAge() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int currentYear = calendar.get(Calendar.YEAR);
+        calendar.setTime(getDateOfBirthday());
+        int year = calendar.get(Calendar.YEAR);
+        return currentYear - year;
+    }
+}
